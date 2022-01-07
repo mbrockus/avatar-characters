2df6bd028cfebbb450f84a153044c2ab1986b05d
mbrockus <matthew.brockus@gmail.com>
2022-01-07 09:54:39 -0500

    added first image

--- src/Components/Body/CardList/Card/Card.js
+++ src/Components/Body/CardList/Card/Card.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import Image from './Image.js';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function Card(props) {
+
+
+    useEffect(() => {
+			fetchData();
+		}, []);
+
+    function fetchData(event) {
+			fetch('https://last-airbender-api.herokuapp.com/api/v1/characters')
+				.then((res) => res.json())
+				.then((data) => console.log(data))
+				.catch((err) => console.error(`Oops, something went wrong: ${err}`));
+		}
+
     return (
         <div>
             <Image/>
